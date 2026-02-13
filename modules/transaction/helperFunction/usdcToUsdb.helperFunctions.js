2c76cc48acfa415fcd0b2d702169b1f2b352dc32
FahadIqbal <fahadiqbalhbd@gmail.com>
2026-02-13 11:02:24 +0500

    url

--- modules/transaction/helperFunction/usdcToUsdb.helperFunctions.js
+++ modules/transaction/helperFunction/usdcToUsdb.helperFunctions.js
@@ -10,6 +10,10 @@ import fs from "fs";
 import { randomHookData } from "../../../utils/utils.js";
 import { Tokens } from "../model/token.model.js";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const USDC_TO_USDB = async (user, amount, to) => {
   try {
